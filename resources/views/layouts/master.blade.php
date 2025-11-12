3b24b97b6005654ed98c6cd02f288a1f0d64e3cc
3B232005 <3B232005@gm.student.ncut.edu.tw>
2025-11-13 02:25:30 +0800

    建立共用區塊

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,8 +1,6 @@
-@extends('layouts.master')
-
-@section('title', 'Home')
-
-@section('content')
-    <h1>Home</h1>
-    <p>This is my body content.</p>
-@endsection
+<body>
+@include('layouts.partials.navigation')
+<divclass="container">
+@yield('content')
+</div>
+</body>
